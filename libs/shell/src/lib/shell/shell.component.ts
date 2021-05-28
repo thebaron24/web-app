54a850fd2aff7d40d58acbd8d5d0e25aca74f540
wilsonb7 <baron.wilson@adp.com>
2021-05-28 15:40:59 -0400

    updated angular version, nx version, and removed non server side rendering friendly code

--- libs/shell/src/lib/shell/shell.component.ts
+++ libs/shell/src/lib/shell/shell.component.ts
@@ -1,13 +1,11 @@
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
-import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'web-app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss']
 })
-export class ShellComponent implements OnDestroy {
-  mobileQuery: MediaQueryList;
+export class ShellComponent {
   showLabels = true;
 
   navItems = [
@@ -18,21 +16,14 @@ export class ShellComponent implements OnDestroy {
     }
   ];
 
-  private readonly _mobileQueryListener: () => void;
 
-  constructor(private changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => this.changeDetectorRef.detectChanges();
-    this.mobileQuery.addEventListener('change', this._mobileQueryListener);
-  }
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
 
   public toggleLabels(): void {
     this.showLabels = !this.showLabels;
     this.changeDetectorRef.detectChanges();
-  }
-
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
 }
