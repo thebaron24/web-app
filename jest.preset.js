b583f6c5c218443b764e64d5797098e07aa1c1db
thebaron24 <thebaron24@gmail.com>
2021-07-17 22:12:42 -0400

    updated nrwl and jest

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,6 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  testRunner: "jest-jasmine2" //temp fox for https://github.com/ike18t/ng-mocks/issues/610
+};
